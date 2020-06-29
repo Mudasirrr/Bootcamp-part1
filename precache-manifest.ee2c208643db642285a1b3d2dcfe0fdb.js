54dcb4dedd414dae15f6518988a9cd55482eb062
Mudasirrr <hmudasir714@email.com>
2020-06-29 07:59:51 +0500

    Updates

---
+++ precache-manifest.ee2c208643db642285a1b3d2dcfe0fdb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2ec1e9016cc50c643f08816e07ba80b",
+    "url": "/Bootcamp-part1/index.html"
+  },
+  {
+    "revision": "ad3de2561b680c262fbe",
+    "url": "/Bootcamp-part1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a9d556f8c08c06351568",
+    "url": "/Bootcamp-part1/static/js/2.8adedac1.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/Bootcamp-part1/static/js/2.8adedac1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad3de2561b680c262fbe",
+    "url": "/Bootcamp-part1/static/js/main.f370b088.chunk.js"
+  },
+  {
+    "revision": "bd3d58fc683babcca638",
+    "url": "/Bootcamp-part1/static/js/runtime-main.69d7c54b.js"
+  }
+]);
